4083bcf55ec99e6827801dc328c870a45ea44a1f
KravchukKat <ka.tarina@mail.ru>
2025-03-26 22:39:30 +0300

    done!

--- src/test/java/GitHubDragDropTests.java
+++ src/test/java/GitHubDragDropTests.java
@@ -1,4 +1,5 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.DragAndDropOptions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.text;
@@ -30,14 +31,14 @@ public class GitHubDragDropTests {
     @Test
     void DragDropActions() {
         open("https://the-internet.herokuapp.com/drag_and_drop");
-        actions().moveToElement($("#column-a")).clickAndHold().moveByOffset("#column-b").release().perform();
+        actions().moveToElement($("#column-a")).clickAndHold().moveToElement($("#column-b")).release().perform();
         $("#column-b").shouldHave(text("A"));
         $("#column-a").shouldHave(text("B"));
     }
     @Test
     void DragDrop() {
         open("https://the-internet.herokuapp.com/drag_and_drop");
-        $("#column-a").dragAndDrop($("#column-b"));
+        $("#column-a").dragAndDrop(DragAndDropOptions.to("#column-b"));
         $("#column-b").shouldHave(text("A"));
         $("#column-a").shouldHave(text("B"));
     }
